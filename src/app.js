b1eb0e83b8d3586ed41c3d5934cae8b90942f7a9
Joao Jose Rocha de Sousa <joaojosers@hotmail.com>
2023-10-06 09:30:56 -0300

    debubando lint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
-const fs = require('fs').promises; // fs = file system
+const fs = require('fs').promises;
 const { join } = require('path');
+const express = require('express');
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
 
 const readTalkerManager = async () => {
   const path = './talker.json';
@@ -17,71 +23,26 @@ const getAllTalkerManager = async () => {
   return talkerManager;
 };
 
-app.get('/talker', async (req, res) => {
+router.get('/talker', async (req, res) => {
   console.log('LISTANDO TODOS OS PALESTRANTES.');
   const talkerManager = await getAllTalkerManager();
   if (talkerManager) return res.status(200).json(talkerManager);
   return res.status(404).json([]);
 });
 
-// app.get('/talker', (req, res) => {
-//     console.log('LISTANDO TODOS OS PALESTRANTES.');
-//     if (talkmanager) return res.status(200).json(talkmanager);
-//     return res.status(404).json([]);
-//   });
+router.get('/talker/:id', async (req, res) => {
+  const talkerId = Number(req.params.id);
+  console.log(`BUSCANDO PALESTRANTE COM ID: ${talkerId}`);
+  
+  const talkerManager = await getAllTalkerManager();
+  const talker = talkerManager.find((e) => e.id === talkerId);
+  
+  if (talker) {
+    return res.status(200).json(talker);
+  } 
+  return res.status(404).json({
+    message: 'Pessoa palestrante não encontrada',
+  });
+});
 
-// app.post('/tasks', (req, res) => {
-//   console.log('CADASTRANDO UMA NOVA TAREFA');
-
-//   const { body } = req;
-//   const nextId = activities.length + 1;
-//   activities.push({ id: nextId, ...body });
-
-//   return res.status(201).json({ message: 'TAREFA CADASTRADA COM SUCESSO' });
-// });
-
-// app.get('/tasks/:taskId', (req, res) => {
-//   console.log('LISTANDO UMA UNICA TAREFA');
-//   const { taskId } = req.params;
-//   console.log(`BUSCANDO A TAREFA ${taskId}`);
-
-//   const task = activities.find((a) => a.id === Number(taskId));
-//   if (!task) {
-//     return res.status(404).json({ message: 'Tarefa não existe' });
-//   }
-
-//   return res.status(200).json(task);
-// }); // req.params
-
-// app.delete('/tasks/:taskId', (req, res) => {
-//   console.log('DELETANDO UMA TAREFA');
-//   const { taskId } = req.params;
-
-//   const taskIndice = activities.findIndex((task) => task.id === Number(taskId));
-//   activities.splice(taskIndice, 1);
-
-//   return res.status(204).end();
-// });
-
-// app.put('/tasks/:taskId', (req, res) => {
-//   console.log('atualizando tarefa');
-//   const { description, status, priority } = req.body;
-//   const { taskId } = req.params;
-
-//   for (let i = 0; i < activities.length; i++) {
-//     const activity = activities[i];
-
-//     if (activity.id === Number(taskId)) {
-//       activity.description = description;
-//       activity.status = status;
-//       activity.priority = priority;
-//     }
-//   }
-
-//   return res.status(204).end();
-// });
-
-module.exports = app;
-
-// no caso deixar a conexão aberta enquanto ainda estiver usando não evitaria esse
-// tipo de erro no terminal?
+module.exports = { app, router };
--- src/index.js
+++ src/index.js
@@ -1,123 +1,16 @@
-const fs = require('fs').promises; // fs = file system
-const { join } = require('path');
 const express = require('express');
-
-const app = express();
-app.use(express.json());
+const { app } = require('./app');
 
 const router = express.Router();
-const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+const HTTP_OK_STATUS = 200;
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
-});
-
-const readTalkerManager = async () => {
-  const path = './talker.json';
-
-  try {
-    const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
-    return JSON.parse(contentFile);
-  } catch (error) {
-    return null;
-  }
-};
-
-const getAllTalkerManager = async () => {
-  const talkerManager = await readTalkerManager();
-  return talkerManager;
-};
-
-app.get('/talker', async (req, res) => {
-  console.log('LISTANDO TODOS OS PALESTRANTES.');
-  const talkerManager = await getAllTalkerManager();
-  if (talkerManager) return res.status(200).json(talkerManager);
-  return res.status(404).json([]);
-});
-
-app.get('/talker/:id', async (req, res) => {
-  // const talkerId = +req.params.id; // Obtém o ID da rota como um número inteiro
-  const talkerId = Number(req.params.id); // Obtém o ID da rota como um número inteiro
-
-  console.log(`BUSCANDO PALESTRANTE COM ID: ${talkerId}`);
-  
-  const talkerManager = await getAllTalkerManager();
-  
-  const talker = talkerManager.find((e) => e.id === talkerId);
-  
-  if (talker) {
-    return res.status(200).json(talker);
-  } 
-  return res.status(404).json({
-    message: 'Pessoa palestrante não encontrada',
-  });
-});
-
-const generateRandomToken = () => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let token = '';
-  for (let i = 0; i < 16; i += 1) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    token += characters[randomIndex];
-  }
-  return token;
-};
-
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  const token = generateRandomToken();
-  console.log(email);
-  console.log(password);
-  res.status(200).json({ token });
-});
-
-const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-
-const validateLoginRequest = (req, res, next) => {
-  const { email, password } = req.body;
-  const errors = {
-    'O campo "email" é obrigatório': !email || email.trim() === '',
-    'O "email" deve ter o formato "email@email.com"': !validateEmail(email),
-    'O campo "password" é obrigatório': !password || password.trim() === '',
-    'O "password" deve ter pelo menos 6 caracteres': password.length < 6,
-  };
-
-  const errorMessages = Object.entries(errors)
-    .filter(([_, condition]) => condition)
-    .map(([_, message]) => message);
-
-  if (errorMessages.length > 0) {
-    return res.status(400).json({ message: errorMessages[0] });
-  }
-
-  next();
-};
-
-router.post('/login', validateLoginRequest, (req, res) => {
-  const token = generateRandomToken();
-  res.status(200).json({ token });
-});
-
-app.use(express.json());
 app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-module.exports = {
-  getAllTalkerManager,
-  // getChocolateById,
-  // getChocolatesByBrand,
-  // readCacauTrybeFile,
-  // writeCacauTrybeFile,
-  // createChocolate,
-  // deleteChocolate,
-};
